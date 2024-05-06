a699467fd9e95a1c6544131a943c0c8d73d554ad
Xavier <xnavarro@ucsd.edu>
2024-05-05 17:01:25 -0700

    trying to add the options locally in init function. not done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,5 +4,19 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  
+  // get all of the aspects from the html
+  // button
+  const talkButton = document.querySelector("button");
+  // text box
+  const talkTextBox = document.querySelector("textarea")
+  // image
+  const talkImage = document.querySelector("img");
+  // dropbox
+  const talkSelection = document.querySelector("select");
 
+  // adds the options to dropbox
+  talkSelection.addEventListener('DOMContentLoaded', function() {
+
+  });
 }
