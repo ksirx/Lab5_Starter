978604acdc6580adce6cfcaa02a649feb5e12bb5
Xavier <xnavarro@ucsd.edu>
2024-05-05 19:52:20 -0700

    created some unit tests in unit.test.js

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,23 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+//false
+test('adds 9 + 10 to equal 21', () => {
+  expect(9 + 10).toBe(21);
+});
+
+//false
+test('adds 9 and 10 equal to each other', () => {
+  expect(9 == 10).toBe(true);
+});
+
+//true
+test('adds 10 and 10 equal to each other', () => {
+  expect(10 == 10).toBe(true);
+});
+
+//true
+test('21 is greater than 100 when doing string comparison', () => {
+  expect("21" > "100").toBe(true);
+});
