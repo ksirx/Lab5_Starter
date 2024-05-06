e197aaeb95cb76bd88c7f24cc34a61ad910d1208
Xavier <xnavarro@ucsd.edu>
2024-05-05 17:59:25 -0700

    got the voices to get added to the dropbox

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,10 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
   // TODO
   
-  // get all of the aspects from the html
+  // get all of the aspects from the html and the synthisizer
+  // init the speech synthisizer
+  const synth = window.speechSynthesis;
   // button
   const talkButton = document.querySelector("button");
   // text box
@@ -15,8 +18,32 @@ function init() {
   // dropbox
   const talkSelection = document.querySelector("select");
 
-  // adds the options to dropbox
-  talkSelection.addEventListener('DOMContentLoaded', function() {
+   // adds all available voices to the dropbox
+   populateVoiceList();
+   if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+   ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+   }
 
-  });
+   // speaks when the button is clicked
+   
+
+   // if speaking then change the image to the speaking image
+ }
+ 
+ function populateVoiceList() {
+  if (typeof speechSynthesis === "undefined") {
+    return;
+  }
+ 
+  const voices = speechSynthesis.getVoices();
+  const talkSelection = document.querySelector("select");
+ 
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = voices[i].name;
+    talkSelection.appendChild(option);
+  }
 }
